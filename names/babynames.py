36a44fb7dce82a0d7ef86c10823b2c06e603476a
Stephen <stephen@umw.edu>
2022-09-13 11:15:09 -0400

    OHE all features at once (meaningful feature names).

--- names/babynames.py
+++ names/babynames.py
@@ -13,19 +13,22 @@ ohe = OneHotEncoder(sparse=False)    # We want "normal" NumPy matrices
 
 # Feature #1: does the name start with a vowel?
 first_letter_type = dataset.Name.str[0].isin(['A','E','I','O','U'])
-ohefirst_letter_type = ohe.fit_transform(
-    first_letter_type.to_numpy().reshape(-1,1))
-first_letter_type_features = ohe.get_feature_names_out()
+first_letter_type = first_letter_type.to_numpy().reshape(-1,1)
 
 # Feature #2: what is the last letter?
 last_letter = dataset.Name.str[-1]
-ohelast_letter = ohe.fit_transform(last_letter.to_numpy().reshape(-1,1))
-last_letter_features = ohe.get_feature_names_out()
+last_letter = last_letter.to_numpy().reshape(-1,1)
+
+# We'll one-hot encode our categorical features all at once. This will give
+# each feature a unique "name" (via .get_feature_names_out()) and also allow us
+# to encode live data as it comes down the pike.
+ohe_features = ohe.fit_transform(np.c_[first_letter_type, last_letter])
+ohe_feature_names = ohe.get_feature_names_out()
 
 # Feature #3: how long is the name?
 num_letters = dataset.Name.str.len().to_numpy().reshape(-1,1).astype(float)
 
-X = np.c_[ohefirst_letter_type, ohelast_letter, num_letters]
+X = np.c_[ohe_features, num_letters]
 y = dataset.Sex.to_numpy()
 
 Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, train_size=.8,
@@ -38,8 +41,7 @@ print(f"Score: {lr.score(Xtest,ytest)}")
 
 # Optional: as a sanity check, let's make a DataFrame that has the name and all
 # the encoded features.
-feature_names = np.concatenate([first_letter_type_features,
-    last_letter_features, ['num_letters']])
+feature_names = np.concatenate([ohe_feature_names, ['num_letters']])
 encoded = pd.DataFrame(X)
 encoded.columns = feature_names
 encoded['Name'] = dataset.Name
