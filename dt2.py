f385b8245b7a5a26c8b3b7ff62d712636537f643
Stephen <stephen@umw.edu>
2022-10-27 11:53:40 -0400

    Finish DT classification examples (Sith/Jedi).

--- dt2.py
+++ dt2.py
@@ -1,4 +1,4 @@
-# Decision trees with all categorical variables.
+# Decision trees with some numeric variables (height, in this case).
 
 import numpy as np
 import pandas as pd
@@ -14,16 +14,15 @@ from sklearn.pipeline import make_pipeline
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import export_graphviz
 
-np.set_printoptions(precision=3)
-
 BASE_FILENAME = "sc_tree"
-sc = pd.read_csv("security_cam.csv")
+sc = pd.read_csv("security_cam2.csv")
 
 pipe = make_pipeline(make_column_transformer(
-    (OrdinalEncoder(categories=[['beard','clean'],['calm','menacing'],
-        ['short','medium','tall']]), ['face','demeanor','height']),
-    (OneHotEncoder(), ['lightsaber'])),
-    DecisionTreeClassifier(max_depth=2,criterion='entropy'))
+    (OrdinalEncoder(categories=[['beard','clean'],['calm','menacing']]),
+        ['face','demeanor']),
+    (OneHotEncoder(), ['game','lightsaber']),
+    ('passthrough', ['height'])),
+    DecisionTreeClassifier(max_depth=3,criterion='entropy'))
 pipe.fit(sc.drop('type',axis=1),sc['type'].to_numpy())
 dot = export_graphviz(pipe.steps[1][1],out_file=None,
     feature_names=pipe.steps[0][1].get_feature_names_out(),
@@ -42,7 +41,7 @@ for value_list in pipe.steps[0][1].transformers_[0][1].categories_:
 print(f"\nFeature importances: {pipe.steps[1][1].feature_importances_}\n")
 
 # Run some tests on sample points.
-sc_test_pts = pd.read_csv("security_cam_test.csv")
+sc_test_pts = pd.read_csv("security_cam2_test.csv")
 sc_test_pts['pred'] = pipe.predict(sc_test_pts.drop('type',axis=1))
 print(sc_test_pts)
 print(pipe.predict_proba(sc_test_pts.drop('type',axis=1)))
