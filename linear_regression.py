ceb6d9e2c6534a4c62e5015941e0aa5de7f119cb
Stephen Davies <stephen@umw.edu>
2026-09-17 15:36:45 -0400

    Complete Linear Reg ex, including use of real data.

--- linear_regression.py
+++ linear_regression.py
@@ -4,18 +4,18 @@ import matplotlib.pyplot as plt
 import scipy.stats
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from wnba import load
+
+def normalize(x):
+    return (x - x.min()) / (x.max() - x.min())
 
 # Fix the random number seed for reproducibility.
-np.random.seed(123)
+np.random.seed(1234)
 
-# The number of data points to generate.
-N = 50
-
-# Here the secretive universe generates x points, and y points based on them.
-# The "true f" is f(x) = 44.6 + 21.9x - 7.1x^2 plus some random "noise" with
-# standard deviation 1.
-x = np.random.uniform(0, 2, N)
-y = 44.6 + 21.9 * x - 7.1 * x**2 + np.random.normal(0, 1, N)
+pstats = load()['pstats']
+x = pstats['min']
+x = normalize(x)
+y = pstats['fgm']
 
 # Plot the points.
 plt.scatter(x, y)
@@ -26,7 +26,7 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2)
 
 # The highest degree polynomial we will experiment with. This is necessary in
 # order to create an X matrix of the right size.
-max_degree = 5
+max_degree = 35
 
 # For both training and test, create a matrix of the correct size.
 X_train = np.zeros((len(x_train), max_degree+1))
@@ -41,17 +41,17 @@ X_test = np.zeros((len(x_test), max_degree+1))
 #    daphne = x_train               ... which is x_train ** 1
 #    X_train[:,1] = daphne
 #    daphne2 = x_test               ... which is x_test ** 1
-#    X_test[:,0] = daphne2
+#    X_test[:,1] = daphne2
 #
 #    addai = x_train ** 2
 #    X_train[:,2] = addai
 #    addai2 = x_test ** 2
-#    X_test[:,0] = addai2
+#    X_test[:,2] = addai2
 #
 #    logan = x_train ** 3
 #    X_train[:,3] = logan
 #    logan2 = x_test ** 3
-#    X_test[:,0] = logan2
+#    X_test[:,3] = logan2
 # Generalize these steps and do it all in one for loop.
 for i in range(0, max_degree+1):
     general_thing = x_train ** i
@@ -59,15 +59,45 @@ for i in range(0, max_degree+1):
     general_thing2 = x_test ** i
     X_test[:,i] = general_thing2
 
-# Create and "fit" a linear regression model. sklearn does the heavy lifting of
-# figuring out the optimal coefficients for a model of given complexity, in
-# this case a degree-1 polynomial (why degree-1 instead of degree-5, which was
-# max_degree? Because below we only give the first two columns of the design
-# matrix X to the model for training.)
-lr = LinearRegression(fit_intercept=False)
-lr.fit(X_train[:,:2], y_train)
+test_mses = np.empty(max_degree+1)
+train_mses = np.empty(max_degree+1)
+test_Rsqs = np.empty(max_degree+1)
+train_Rsqs = np.empty(max_degree+1)
+for degree in range(0, max_degree+1):
+    # Create and "fit" a linear regression model. sklearn does the heavy
+    # lifting of figuring out the optimal coefficients for a model of given
+    # complexity, in this case a degree-1 polynomial (why degree-1 instead of
+    # degree-5, which was max_degree? Because below we only give the first two
+    # columns of the design matrix X to the model for training.)
+    lr = LinearRegression(fit_intercept=False)
+    lr.fit(X_train[:,:degree+1], y_train)
 
-print(f"The least-squares coefficients are {lr.coef_}")
+    print(f"The least-squares coefficients are {lr.coef_}")
 
-# Add the best-fit line to the plot, using the coefficients.
-#plt.plot(X_train[:,1], ..???..)
+    train_preds = lr.predict(X_train[:,:degree+1])
+    train_mse = ((y_train - train_preds)**2).mean()
+    train_mses[degree] = train_mse
+    train_Rsqs[degree] = lr.score(X_train[:,:degree+1], y_train)
+    test_preds = lr.predict(X_test[:,:degree+1])
+    test_mse = ((y_test - test_preds)**2).mean()
+    test_mses[degree] = test_mse
+    test_Rsqs[degree] = lr.score(X_test[:,:degree+1], y_test)
+    print(f"degree {degree} MSE: train {train_mse:.3f}, test {test_mse:.3f}")
+    
+    # Add the best-fit line to the plot, using the coefficients.
+    x_plot = np.linspace(0, x_train.max(), 175)
+    y_plot = 0
+    for i in range(0, degree+1):
+        y_plot += lr.coef_[i] * x_plot ** i
+    plt.plot(x_plot, y_plot)
+
+
+fig, ax = plt.subplots()
+#ax.plot(train_mses, label="training MSE", color="green")
+#ax.plot(test_mses, label="test MSE", color="red")
+ax.plot(train_Rsqs, label="training R^2", color="green")
+ax.plot(test_Rsqs, label="test R^2", color="red")
+ax.legend()
+
+
+
